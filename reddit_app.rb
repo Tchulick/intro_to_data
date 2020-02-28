007b68b7664cbf416c11283b68420814969f128e
Tom <tchulick48@gmail.com>
2020-02-27 21:32:05 -0600

    title selector test

--- reddit_app.rb
+++ reddit_app.rb
@@ -1,17 +1,19 @@
 require "http"
 
+require "tty-prompt"
+
+prompt = TTY::Prompt.new
+
 response = HTTP.get("https://www.reddit.com/r/photography/.json")
 page_info = response.parse
 
 i = 0
+page_array = []
 while i < page_info["data"]["children"].length
   title = page_info["data"]["children"][i]["data"]["title"]
-  puts "#{i + 1}. #{title}"
+  # puts "#{i + 1}. #{title}"
+  page_array << title
   i += 1
 end
 
-# require "tty-prompt"
-
-# prompt = TTY::Prompt.new
-
-# prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
+prompt.select("Which page would you like to visit?", page_array)
