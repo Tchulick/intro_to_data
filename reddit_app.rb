e9bc3543c639226d52f979232819d704e5446a6f
Tom <tchulick48@gmail.com>
2020-02-27 21:21:20 -0600

    searching through reddit page

--- reddit_app.rb
+++ reddit_app.rb
@@ -1,5 +1,17 @@
-require "tty-prompt"
+require "http"
 
-prompt = TTY::Prompt.new
+response = HTTP.get("https://www.reddit.com/r/photography/.json")
+page_info = response.parse
 
-prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
+i = 0
+while i < page_info["data"]["children"].length
+  title = page_info["data"]["children"][i]["data"]["title"]
+  puts "#{i + 1}. #{title}"
+  i += 1
+end
+
+# require "tty-prompt"
+
+# prompt = TTY::Prompt.new
+
+# prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
